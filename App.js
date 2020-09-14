a8ca144ef932bbf3a621beb9f52243c03910d197
Seba <sebastiancura97@gmail.com>
2020-09-14 19:44:31 -0300

    Remove barcodes

--- App.js
+++ App.js
@@ -3,32 +3,32 @@ import { StyleSheet, View, Alert } from 'react-native'
 import { RNCamera } from 'react-native-camera'
 
 class App extends Component {
-  state = {
-    barcodes: []
-  }
+  // state = {
+  //   barcodes: []
+  // }
 
-  barcodeRecognized = ({ barcodes }) => {
-    barcodes.forEach(barcode => console.log(barcode.data))
-    this.setState({ barcodes })
-  }
+  // barcodeRecognized = ({ barcodes }) => {
+  //   barcodes.forEach(barcode => console.log(barcode.data))
+  //   this.setState({ barcodes })
+  // }
 
-  renderBarcodes = () => (
-    <View>{this.state.barcodes.map(this.renderBarcode)}</View>
-  )
+  // renderBarcodes = () => (
+  //   <View>{this.state.barcodes.map(this.renderBarcode)}</View>
+  // )
 
-  renderBarcode = ({ data }) =>
-    Alert.alert(
-      'Scanned Data',
-      data,
-      [
-        {
-          text: 'Okay',
-          onPress: () => console.log('Okay Pressed'),
-          style: 'cancel'
-        }
-      ],
-      { cancelable: false }
-    )
+  // renderBarcode = ({ data }) =>
+  //   Alert.alert(
+  //     'Scanned Data',
+  //     data,
+  //     [
+  //       {
+  //         text: 'Okay',
+  //         onPress: () => console.log('Okay Pressed'),
+  //         style: 'cancel'
+  //       }
+  //     ],
+  //     { cancelable: false }
+  //   )
 
   render() {
     return (
@@ -38,8 +38,9 @@ class App extends Component {
             this.camera = ref
           }}
           style={styles.scanner}
-          onGoogleVisionBarcodesDetected={this.barcodeRecognized}>
-          {this.renderBarcodes}
+          // onGoogleVisionBarcodesDetected={this.barcodeRecognized}
+          >
+          {/* {this.renderBarcodes} */}
         </RNCamera>
       </View>
     )
